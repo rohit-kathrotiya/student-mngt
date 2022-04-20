ab9cac84c8d88900dca07dc4c1539627f5aea4b3
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-04-20 21:20:59 +0200

    create Form for student by using New and Create actions and perform Validations and print error messages

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,23 @@ class StudentsController < ApplicationController
     def index
         @students = Student.all
     end
+
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.new(student_params)
+        if @student.save
+            redirect_to students_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name, :email, :city)
+    end
 end
