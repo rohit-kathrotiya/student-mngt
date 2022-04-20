64cbebdc89beca49a9f9d170236e18fbb548f1d0
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-04-20 17:03:34 +0200

    create student controller and provide resources in routes file

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,6 @@
+class StudentsController < ApplicationController
+
+    def index
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   
   root 'welcome#index'
 
+  resources :students
+
 end
