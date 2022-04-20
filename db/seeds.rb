c2c48cf05ade57d50fc52081b295680b3d242a5d
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-04-20 17:04:05 +0200

    Enter data in student table by seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+student = Student.create(
+    first_name: "Rohit",
+    last_name: "kathrotiya",
+    email: "rohit@gmail.com",
+    city: "Surat"
+)
+
+# THIS IS SAME METHOD WHICH DESCRIBE BEFORE.
+# student = Student.new(
+#     first_name: "Rohit",
+#     last_name: "kathrotiya",
+#     email: "rohit@gmail.com",
+#     city: "Surat"
+# )
+
+# student.save()
+
+15.times do |i|
+    Student.create(
+        first_name: "Rohit #{i+1}",
+        last_name: "Kathrotiya #{i+1}",
+        email: "rohit #{i+1}@gmail.com",
+        city: "Surat #{i+1}"
+    )
+end
