90df7f47555c335ebd48c01fa8acfa732dba14c7
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-04-21 16:41:30 +0200

    Update student by using edit and update actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,6 +21,19 @@ class StudentsController < ApplicationController
         @student = Student.find(params[:id])
     end
 
+    def edit
+        @student = Student.find(params[:id])
+    end
+
+    def update
+        @student = Student.find(params[:id])
+        if @student.update(student_params)
+            redirect_to student_path(@student)
+        else
+            render :edit
+        end
+    end
+
     private
 
     def student_params
