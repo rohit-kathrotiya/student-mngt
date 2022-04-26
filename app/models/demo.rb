785af97cb440c8bed17c2855e42ddbd261e47669
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-04-26 18:15:02 +0200

    Perform Active_record:callbacks in student and demo model

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -1,3 +1,9 @@
 class Demo < ApplicationRecord
     has_one  :sub_demo
+
+    before_create :greeting_callback
+
+    def greeting_callback
+        puts "before craete callback work coreecttly.."
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,15 @@ class Student < ApplicationRecord
     has_many :student_projects
     has_many :projects, through: :student_projects
     validates :first_name, :last_name, :email, :city, presence: true
+
+    after_save :display_student_age
+
+    def display_student_age
+        if self.date_of_birth.present?
+            age = Date.today.year - self.date_of_birth.year
+            puts "Total age of student is : #{age}"
+        else
+            puts "Age can not be calculated withpout age"
+        end
+    end
 end
