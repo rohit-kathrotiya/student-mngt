7652c77383585e211c64170289dc9bd1fdd0a9da
Rohit Kathrotiya <rohitkathrotiya63@gmail.com>
2022-05-06 17:35:12 +0200

    Craete helper method into controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
     #  It create an method for all view layout. if you want to use thid method in controller then simply
     #  include this file in controller.
-    def formate_date(date)
-        date.strftime('%A, %b %d, %Y') if date.present?
-    end
+    # def formate_date(date)
+    #     date.strftime('%A, %b %d, %Y') if date.present?
+    # end
 end
